cd7b11a148217fc67a1a8f03aa16cceb5581df8f
KDragon <elkarouani@gmail.com>
2019-01-06 14:53:15 +0100

    Task: made basic autocompliting text with basic suggestions

--- src/AutoCompleteText.js
+++ src/AutoCompleteText.js
@@ -9,15 +9,41 @@ export default class AutoCompleteText extends React.Component {
 			'Sara',
 			'Jane',
 		]
+
+		this.state = {
+			suggestions: [],
+		};
+	}
+
+	onTextChanged = (e) => {
+		const value = e.target.value;
+		let suggestions = [];
+		if (value.length > 0) {
+			const regex = new RegExp(`^${value}`, `i`);
+			suggestions = this.items.sort().filter(item => regex.test(item));
+		}
+		
+		this.setState(() => ({ suggestions }));
+	}
+
+	renderSuggestions() {
+		const { suggestions } = this.state;
+		if (suggestions.length === 0) {
+			return null;
+		}
+
+		return (
+			<ul>
+				{suggestions.map((item) => <li>{item}</li>)}
+			</ul>
+		)
 	}
 
 	render() {
 		return (
 			<div>
-				<input onChange={(e) => console.log(e.target.value)} type="text"/>
-				<ul>
-					{this.items.map((item) => <li>{item}</li>)}
-				</ul>
+				<input onChange={this.onTextChanged} type="text"/>
+				{this.renderSuggestions()}
 			</div>
 		);
 	}
