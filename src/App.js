f57a50dc87404c3b7a9d44144c7ab13ab505fb82
KDragon <elkarouani@gmail.com>
2019-01-06 13:58:42 +0100

    Playing with hideable text

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import HideableText from './HideableText';
 
 class App extends Component {
   render() {
@@ -9,17 +10,12 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Welcome To React
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
+        <div className="App-content">
+          <HideableText text="Dynamic Text!" />
+        </div>
       </div>
     );
   }
